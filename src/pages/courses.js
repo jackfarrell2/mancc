f127b02ae49595412060d0f6c242765e6f5fa570
Jack Farrell <jackfarrell860@gmail.com>
2023-11-22 17:50:18 -0500

    Started courses page

--- src/pages/courses.js
+++ src/pages/courses.js
@@ -1,12 +1,53 @@
-import { Typography, Box } from "@mui/material"
+import * as React from 'react'
+import { Grid, Box } from "@mui/material"
 import { page } from '../styles/classes'
+import { GolferSelect } from "../components/GolferSelect"
+import { LoadingSpinner } from '../components/LoadingSpinner'
 
 function Courses() {
-    return (
-        <Box sx={page}>
-            <Typography>This is going to be the Courses page!</Typography>
-        </Box>
-    )
+
+    const [golfer, setGolfer] = React.useState('Nick')
+    const [course, setCourse] = React.useState('Manchester Country Club')
+    const [tees, setTees] = React.useState('White')
+    const [golfers, setGolfers] = React.useState(null)
+
+    const URL = `http://127.0.0.1:8000/api/courses/${course}/${tees}/${golfer}`
+
+    React.useEffect(() => {
+        const fetchData = async () => {
+            const result = await fetch(URL)
+            result.json().then(json => {
+                console.log(json)
+            })
+        }
+        fetchData();
+    }, [])
+
+    if (!golfers) {
+        return (
+            <Box sx={page}>
+                <Grid container justifyContent="center" alignItems="center">
+                    <Grid item>
+                        <LoadingSpinner />
+                    </Grid>
+                </Grid>
+            </Box>
+        )
+    } else {
+         return (
+            <Box sx={page}>
+                <Grid container direction="column" justifyContent="center" alignItems="flex-start" spacing={2}>
+                    <Grid item>
+                        <Grid container direction="row" justifyContent="center" alignItems="center" spacing={2}>
+                            <Grid item>
+                                <GolferSelect golfer={golfer} golfers={golfers}/>
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                </Grid>
+            </Box>
+        )
+    }
 }
 
 export default Courses;
