3fc76e985d540c05d0ba89f584ff0d1e5c006ec0
Jack <jackfarrell860@gmail.com>
2023-12-19 07:40:25 -0500

    Fixed dev api config

--- src/config.js
+++ src/config.js
@@ -1,5 +1,9 @@
+const api = process.env.NODE_ENV === 'development'
+  ? process.env.REACT_APP_API_URL_DEV
+  : process.env.REACT_APP_API_URL_PROD;
+
 const config = {
-    apiUrl: process.env.REACT_APP_API_URL || 'http://localhost:8000/'
+    apiUrl: api
 };
 
 export default config;
